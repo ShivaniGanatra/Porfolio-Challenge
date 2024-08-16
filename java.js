4f7d60ec07faec4b811e1ccc007f7870f308afff
ShivaniGanatra <shivaniganatra@live.com>
2024-08-16 16:44:50 +0100

    Changed colours added styling to navigation buttons and project buttons

--- java.js
+++ java.js
@@ -1,39 +1,55 @@
+
 const button = document.querySelector(".nav__button");
 
 let navContainer = document.querySelector(".nav__links");
 
 let navHasBeenOpened = false;
 
+
+let navBurger = document.querySelector(".burger-menu-nav")
+
+// background-color: blue;
+// width: 290px;
+
+
+const headerText = document.querySelector(".header__text")
+
 button.addEventListener("click", () => {
     if (!navHasBeenOpened) {
         navContainer.innerHTML = `
             <div class="nav__link">
             <i class=" nav__item fa fa-chevron-right" style="font-size:20px;color:purple"></i>
-            <a class="nav__item" href="#about-me">About Me</a>
+            <a class="nav__item" href="#about-me">About Me ( )</a>
         </div>
 
         <div class="nav__link">
             <i class=" nav__item fa fa-chevron-right" style="font-size:20px;color:purple"></i>
-            <a class="nav__item" href="#technical-skills">Technical skills</a>
+            <a class="nav__item" href="#technical-skills">Technical skills ( )</a>
         </div>
 
         <div class="nav__link">
             <i class=" nav__item fa fa-chevron-right" style="font-size:20px;color:purple"></i>
-            <a class="nav__item" href="#projects">My Projects</a>
+            <a class="nav__item" href="#projects">My Projects ( )</a>
         </div>
 
         <div class="nav__link">
             <i class=" nav__item fa fa-chevron-right" style="font-size:20px;color:purple"></i>
-            <a class="nav__item" href="#contact-info">Get in Touch</a>
+            <a class="nav__item" href="#contact-info">Get in Touch ( )</a>
         </div>`;
+
+        headerText.style.display = "none";
+        navBurger.style.width = "290px";
+        navBurger.style.background = `lightcyan`;
 
         navHasBeenOpened = true
         console.log(navHasBeenOpened)
     }
 
     else if (navHasBeenOpened) {
+        navBurger.style.width = "50px";
+        headerText.style.display = "block"
         navContainer.innerHTML = ""
-        navHasBeenOpened = false
+        navHasBeenOpened = false;
     }
  
 });
